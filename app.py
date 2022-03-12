ba4f55e0e19fcef72ea7a4454c692f199839e6f9
ashen iranga hewarathna <asheniranga@gmail.com>
2022-03-12 12:46:51 +0530

    add base templates

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def index():
     # put application's code here
     organization = 'Crop Foundation'
-    return f'<h1>{organization}</h1>'
+    return render_template('index.html')
+
+
+@app.route('/planter')
+def planter():
+    return render_template('planter.html')
 
 
 if __name__ == '__main__':
