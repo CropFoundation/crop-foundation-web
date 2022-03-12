9455cfa3a4e6197f08ad96f398517e2c8687fc8b
ashen iranga hewarathna <asheniranga@gmail.com>
2022-03-12 11:49:01 +0530

    version one dev server setup

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello_world():
+    # put application's code here
+    organization = 'Crop Foundation'
+    return f'<h1>{organization}</h1>'
 
 
 if __name__ == '__main__':
